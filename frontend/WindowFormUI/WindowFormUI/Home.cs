994cbad7435ec03f110370718c64e609bb2946fe
Long Vo <vokimlong11.cs@gmail.com>
2024-09-30 21:29:04 +0700

    Add Login, Home, ClassDashboard UI

---
+++ frontend/WindowFormUI/WindowFormUI/Home.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowFormUI
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel1_Paint(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel2_Paint(object sender, EventArgs e)
+        {
+
+        }
+        private void guna2Panel1_MouseHover(object sender, EventArgs e)
+        {
+            guna2Panel1.FillColor = System.Drawing.Color.WhiteSmoke;
+            guna2Panel5.Visible = true;
+        }
+
+        private void guna2Panel1_MouseLeave(object sender, EventArgs e)
+        {
+            guna2Panel1.FillColor = System.Drawing.Color.White;
+            guna2Panel5.Visible = false;
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+     
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label26_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
